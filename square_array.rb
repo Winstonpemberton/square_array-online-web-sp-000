02e47d64719ac9f285257fee202af1de880473f9
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-07 23:38:15 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,11 @@
+require 'pry'
 def square_array(array)
   # your code here
-  new_array =[]
+new_array =[]
   array.each do |number|
     number ** 2
     new_array << number
+    binding.pry
   end
   new_array
 end
