0de1529a82795abd79ce99dfdf565ea6175c931b
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-07 23:57:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,13 @@
 
 def square_array(array)
   # your code here
-new_array =[]
+# new_array =[]
   # array.each do |number|
   #   number ** 2
   #   new_array << number
   #   new_array
   # end
-arr = []
-array.each { |i| arr << i ** 2 }
-arr
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
