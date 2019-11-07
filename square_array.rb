16a17b7924afb5a5c003b787b78b0f4295ab2250
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-07 23:59:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,8 +4,8 @@ def square_array(array)
 new_array =[]
   array.each do |number|
     new_array << number ** 2
-    new_array
   end
+  new_array
   # arr = []
   # array.each { |i| arr << i ** 2 }
   # arr
